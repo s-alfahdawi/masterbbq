9ec546d43b36bd712791e441b3e6bd44680363f0
dhay salih <dhaysalih18@gmail.com>
2024-05-04 23:47:54 +0300

    minor fixes

--- app/_components/home/specials/specials.js
+++ app/_components/home/specials/specials.js
@@ -11,17 +11,6 @@ export default function Specials() {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-y-10 gap-x-20 md:gap-10 ">
                 <div className="flex justify-center">
                     <div className="ribbon-container">
-
-                        <div className="image-container">
-                            <Image src={photo2} alt="specials" className='rounded-[15px]' width={400} height={200}/>
-                        </div>
-
-
-                    </div>
-                </div>
-
-                <div className="flex justify-center">
-                    <div className="ribbon-container">
                         <div className="image-container">
                             <Link href={`/raw-meat`}>
                                 <Image src={photo1} alt="Original Photo" className='rounded-[15px] soon ' width={400}
@@ -30,6 +19,16 @@ export default function Specials() {
                         </div>
                     </div>
                 </div>
+                <div className="flex justify-center">
+                    <div className="ribbon-container">
+                        <div className="image-container">
+                            <Image src={photo2} alt="specials" className='rounded-[15px]' width={400} height={200}/>
+                        </div>
+
+
+                    </div>
+                </div>
+
 
             </div>
         </div>
