4863d19bb6aeb75aaa523e249739286553d06be0
dhay salih <dhaysalih18@gmail.com>
2024-05-04 09:14:28 +0300

    kids meals and raw meat added

--- app/_components/home/video/Video.js
+++ app/_components/home/video/Video.js
@@ -1,10 +1,11 @@
 "use client"
 
+
 export default function VideoComponent() {
 
 
     return (
-        <video className="video-container" width="990" height="500" controls preload="none" loop  muted>
+        <video className="video-container" width="990" height="500" controls preload="none" loop  muted >
             <source src="https://masterbbqerbil.net/videos/bbq.mp4" type="video/mp4"/>
             <track
                 src="https://masterbbqerbil.net/videos/bbq.mp4"
