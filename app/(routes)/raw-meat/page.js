bece01b4e44a3b0727dfef2c2f877b957360adfb
dhay salih <dhaysalih18@gmail.com>
2024-05-05 09:17:48 +0300

    modal added

--- app/(routes)/raw-meat/page.js
+++ app/(routes)/raw-meat/page.js
@@ -1,10 +1,22 @@
 "use client"
 
 import meatData from "@/app/_api/rawmeat.json";
+import {useState} from "react";
 
 const Menu = () => {
     const meat = meatData.meat;
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [isOpen, setIsOpen] = useState(false);
 
+    const handleOpenModal = (image) => {
+        setSelectedImage(image);
+        setIsOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setSelectedImage(null);
+        setIsOpen(false);
+    };
     return (
         <div className="mt-10">
             <div className="hero-content  w-full ml-auto mr-auto text-center text-neutral-content">
@@ -17,7 +29,10 @@ const Menu = () => {
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:gap-x-40 gap-x-30 md:gap-x-20 gap-y-10">
                         {meat.map((rawmeat) => (
                             <div key={rawmeat.id} className="card bg-base-100 shadow-xl max-w-md mx-auto">
-                                <figure className="px-10 pt-10 cursor-pointer">
+                                <figure className="px-10 pt-10 cursor-pointer"
+
+                                        onClick={() => handleOpenModal(rawmeat.photo)}>
+
                                     <img src={rawmeat.photo} alt={rawmeat.name} className="rounded-xl card-img" />
                                 </figure>
                                 <div className="card-body items-center text-center">
@@ -32,7 +47,15 @@ const Menu = () => {
                     </div>
                 </div>
             </div>
-
+            {isOpen && (
+                <div
+                    className="fixed top-0 right-0 bottom-0 md-modal left-0 flex sm:w-30 md:w-30 z-50 justify-center items-center bg-black bg-opacity-40"
+                    onClick={handleCloseModal}>
+                    <div className="max-w-md p-4 bg-base-100  rounded-lg" onClick={(e) => e.stopPropagation()}>
+                        <img src={selectedImage} alt="Selected" className="w-full h-auto"/>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
