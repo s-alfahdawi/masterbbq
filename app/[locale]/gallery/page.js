c418a24e3aeb4cb9c94e0ea4080f48505910ee90
Khalid M. Sheet <khalid.m.sheet@gmail.com>
2024-06-01 00:40:48 +0300

    chore: :star: removed .next & gallery page

--- app/[locale]/gallery/page.js
+++
@@ -1,6 +0,0 @@
-import { general_photos } from "@/app/[locale]/_api/photos.json";
-import Gallery from "/app/_components/gallery/Gallery";
-
-export default function Gallerypage() {
-  return <Gallery photos={general_photos} />;
-}
