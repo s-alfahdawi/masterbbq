cfbb89b59a70a560e8defc7ed6ce7e63125d0869
s-alfahdawi <39900828+s-alfahdawi@users.noreply.github.com>
2024-06-02 11:23:03 +0300

    fix localization v2

--- next.config.js
+++ next.config.js
@@ -1,40 +1,22 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//
-//     images: {
-//         remotePatterns: [
-//             {
-//                 protocol: 'https',
-//                 hostname: 'masterbbqerbil.net',
-//                 pathname: '/**',
-//             },
-//         ],
-//     },
-// };
-//
-// export default nextConfig;
-
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin();
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   i18n: {
-    locales: ['en-US', 'ar', 'kr'],
+    // These are all the locales you want to support in
+    // your application
+    locales: ['ar', 'kr'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. /hello
     defaultLocale: 'ar',
+    // This is a list of locale domains and the default locale they
+    // should handle (these are only required when setting up domain routing)
+    // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "masterbbqerbil.net",
+          pathname: "/**",
+        },
+      ],
+    },
   },
-
-  
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "masterbbqerbil.net",
-        pathname: "/**",
-      },
-    ],
-  },
-};
-
-export default withNextIntl(nextConfig);
+}
